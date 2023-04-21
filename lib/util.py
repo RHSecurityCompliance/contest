5bb3d5831204db4c6df202f786df7d20a2a8f66f
Jiri Jaburek <comps@nomail.dom>
2023-04-21 19:24:31 +0200

    move Beaker/TMT checks to util, make them generic
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- lib/util.py
+++ lib/util.py
@@ -1,3 +1,4 @@
+import os
 import re
 import inspect
 import subprocess
@@ -36,3 +37,18 @@ def proc_stream(cmd, check=False, **kwargs):
             raise subprocess.CalledProcessError(cmd=cmd, returncode=code)
 
     return (proc, generate_lines())
+
+
+def running_in_beaker():
+    """
+    Return True if running in Beaker under the FMF wrapper.
+    """
+    taskpath = os.environ.get('RSTRNT_TASKPATH')
+    return bool(taskpath and taskpath.endswith('/distribution/wrapper/fmf'))
+
+
+def running_in_tmt():
+    """
+    Return True if running under TMT.
+    """
+    return bool(os.environ.get('TMT_TEST_DATA'))
