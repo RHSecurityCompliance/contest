29cd6b11f937ecdf9904583d1dd029d9f6d199a6
Jiri Jaburek <comps@nomail.dom>
2023-04-25 09:56:42 +0200

    use shipped content kickstarts for /hardening/anaconda
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- hardening/anaconda/test.py
+++ hardening/anaconda/test.py
@@ -12,38 +12,42 @@ import versions
 
 virt.setup_host()
 
-profile = os.environ['PROFILE']
-profile = f'xccdf_org.ssgproject.content_profile_{profile}'
-
 g = virt.Guest()
 
-ks = virt.Kickstart()
+profile = os.environ['PROFILE']
 
-# remediate using Anaconda's oscap addon
-oscap_conf = {
-    'content-type': 'scap-security-guide',
-    'profile': profile,
-}
-ks.add_oscap(oscap_conf)
+# use kickstart from content, not ours
+ks = virt.translate_ssg_kickstart(profile)
+
+profile = f'xccdf_org.ssgproject.content_profile_{profile}'
 
 ks.add_post('chage -d 99999 root')
 
 if profile.endswith('_gui'):
     ks.add_package_group('Server with GUI')
 
-g.install(kickstart=ks)
+oscap_conf = {
+    'content-type': 'datastream',
+    'content-url': f'http://{virt.NETWORK_HOST}:8088/contest-ds.xml',
+    'profile': profile,
+}
+ks.add_oscap(oscap_conf)
+
+# host a HTTP server with a datastream and let the guest download it
+virt.firewalld_allow_port(8088)
+srv = util.BackgroundHTTPServer(virt.NETWORK_HOST, 8088)
+srv.add_file(util.get_datastream(), 'contest-ds.xml')
+with srv:
+    g.install(kickstart=ks)
 
 with g.booted():
-    # copy our datastream to the guest
-    g.copy_to(util.get_datastream(), 'contest-ds.xml')
-
     # old RHEL-7 oscap mixes errors into --progress rule names without a newline
     verbose = '--verbose INFO' if versions.oscap >= 1.3 else ''
     redir = '2>&1' if versions.oscap >= 1.3 else ''
 
     # scan the remediated system
     proc, lines = g.ssh_stream(f'oscap xccdf eval {verbose} --profile {profile} --progress '
-                               f'--report report.html contest-ds.xml {redir}')
+                               f'--report report.html /root/openscap_data/contest-ds.xml {redir}')
     failed = oscap.report_from_verbose(lines)
     if proc.returncode not in [0,2]:
         raise RuntimeError("post-reboot oscap failed unexpectedly")
