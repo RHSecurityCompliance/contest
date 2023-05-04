3ab8f2ffe1b6e4b76c376ff87f1a74b92a2ec772
Jiri Jaburek <comps@nomail.dom>
2023-05-04 03:10:39 +0200

    remove CONTEST_* variables, rely on installed ssg RPM
    The motivation for this is that the content set is incomplete -
    when thinking about adding more tests into contest, I realized
    that this is missing at least the equivalent of
    
      - CONTENT_SOURCE, for running automatus
      - CONTENT_VERSION, to replace versions.ssg
    
    whereas if we just stick to an installed RPM, we get both of these
    for free:
    
      - source can be simply downloaded:
          dnf download --source scap-security-guide
          yumdownloader --source scap-security-guide
    
      - version needs no changes, it can use the same RPM logic
    
    Finally, we already have RPM-based workflows in place:
    
      - brew-build based testing in downstream
      - Packit based building/testing in upstream
    
    so there is no need to invent a third way.
    
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- lib/util.py
+++ lib/util.py
@@ -16,29 +16,27 @@ import versions
 #   so we could add the libdir to PATH and PYTHONPATH
 libdir = Path(inspect.getfile(inspect.currentframe())).parent
 
-# content locations
-DATASTREAMS = Path(os.getenv('CONTEST_DATASTREAMS', '/usr/share/xml/scap/ssg/content'))
-PLAYBOOKS = Path(os.getenv('CONTEST_PLAYBOOKS', '/usr/share/scap-security-guide/ansible'))
-KICKSTARTS = Path(os.getenv('CONTEST_KICKSTARTS', '/usr/share/scap-security-guide/kickstart'))
-
 
 def get_datastream():
+    base_dir = Path('/usr/share/xml/scap/ssg/content')
     if versions.rhel:
-        return DATASTREAMS / f'ssg-rhel{versions.rhel.major}-ds.xml'
+        return base_dir / f'ssg-rhel{versions.rhel.major}-ds.xml'
     else:
         raise RuntimeError("cannot find datastream for non-RHEL")
 
 
 def get_playbook(profile):
+    base_dir = Path('/usr/share/scap-security-guide/ansible')
     if versions.rhel:
-        return PLAYBOOKS / f'rhel{versions.rhel.major}-playbook-{profile}.yml'
+        return base_dir / f'rhel{versions.rhel.major}-playbook-{profile}.yml'
     else:
         raise RuntimeError("cannot find playbook for non-RHEL")
 
 
 def get_kickstart(profile):
+    base_dir = Path('/usr/share/scap-security-guide/kickstart')
     if versions.rhel:
-        return KICKSTARTS / f'ssg-rhel{versions.rhel.major}-{profile}-ks.cfg'
+        return base_dir / f'ssg-rhel{versions.rhel.major}-{profile}-ks.cfg'
     else:
         raise RuntimeError("cannot find kickstart for non-RHEL")
 
