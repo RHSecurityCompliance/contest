9d1c2d4c4ae854af10678472009f55276d0e2985
Jiri Jaburek <comps@nomail.dom>
2023-06-20 16:06:09 +0200

    add a /hardening/host-os/ansible test, disabled for now
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

---
+++ hardening/host-os/ansible/test.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+
+from lib import util, results, oscap, versions
+from conf import remediation_excludes
+
+
+profile = os.environ['PROFILE']
+profile_full = f'xccdf_org.ssgproject.content_profile_{profile}'
+
+# the VM guest ssh code doesn't use $HOME/.known_hosts, so Ansible blocks
+# on trying to accept its ssh key - tell it to ignore this
+os.environ['ANSIBLE_HOST_KEY_CHECKING'] = 'False'
+
+# rhc-worker-playbook modules, exported per official instructions on
+# https://access.redhat.com/articles/remediation
+os.environ['ANSIBLE_COLLECTIONS_PATH'] = \
+    '/usr/share/rhc-worker-playbook/ansible/collections/ansible_collections/'
+
+# this is an alternate way to get modules provided by rhc-worker-playbook,
+# a RPM which is not available on CentOS / Fedora - uncomment and use this
+# if we ever run on non-RHEL
+# https://issues.redhat.com/browse/OPENSCAP-2296
+#for collection in ['community.general', 'ansible.posix']:
+#    util.subprocess_run(
+#        ['ansible-galaxy', '-vvv', 'collection', 'install', collection],
+#        check=True)
+
+if util.get_reboot_count() == 0:
+
+    util.log("first boot, remediating using ansible-playbook")
+    playbook = util.get_playbook(profile)
+    skip_tags = ','.join(remediation_excludes.ansible_skip_tags)
+    skip_tags_arg = ['--skip-tags', skip_tags] if skip_tags else []
+    cmd = [
+        'ansible-playbook', '-v', '-c', 'local', '-i', 'localhost,',
+        *skip_tags_arg,
+        playbook,
+    ]
+    util.subprocess_run(cmd, check=True)
+
+    # restore basic login functionality
+    util.subprocess_run(['chage', '-d', '99999', 'root'], check=True)
+    with open('/etc/sysconfig/sshd', 'a') as f:
+        f.write('\nOPTIONS=-oPermitRootLogin=yes\n')
+
+    util.reboot()
+
+else:
+
+    util.log("second boot, scanning")
+
+    # old RHEL-7 oscap mixes errors into --progress rule names without a newline
+    verbose = ['--verbose', 'INFO'] if versions.oscap >= 1.3 else []
+    redir = {'stderr': subprocess.STDOUT} if versions.oscap >= 1.3 else {}
+
+    # scan the remediated system
+    cmd = [
+        'oscap', 'xccdf', 'eval', *verbose, '--profile', profile,
+        '--progress', '--report', 'report.html',
+        util.get_datastream(),
+    ]
+    proc, lines = util.subprocess_stream(cmd, **redir)
+    oscap.report_from_verbose(lines)
+    if proc.returncode not in [0,2]:
+        raise RuntimeError("post-reboot oscap failed unexpectedly")
+
+    results.report_and_exit(logs=['report.html'])
