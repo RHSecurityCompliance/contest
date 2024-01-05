3bdb113de56c4f14807709747b1fd019210ec51d
Jiri Jaburek <comps@nomail.dom>
2024-01-05 11:33:58 +0100

    use correct datastream/playbook paths on CentOS Stream
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- lib/util/content.py
+++ lib/util/content.py
@@ -1,27 +1,38 @@
 from pathlib import Path
 
-from .. import versions
+from ..versions import rhel
 
 
 def get_datastream():
     base_dir = Path('/usr/share/xml/scap/ssg/content')
-    if versions.rhel:
-        return base_dir / f'ssg-rhel{versions.rhel.major}-ds.xml'
+    if rhel.is_true_rhel():
+        return base_dir / f'ssg-rhel{rhel.major}-ds.xml'
+    elif rhel.is_centos():
+        if rhel <= 8:
+            return base_dir / f'ssg-centos{rhel.major}-ds.xml'
+        else:
+            return base_dir / f'ssg-cs{rhel.major}-ds.xml'
     else:
-        raise RuntimeError("cannot find datastream for non-RHEL")
+        raise RuntimeError("cannot find datastream for testing")
 
 
 def get_playbook(profile):
     base_dir = Path('/usr/share/scap-security-guide/ansible')
-    if versions.rhel:
-        return base_dir / f'rhel{versions.rhel.major}-playbook-{profile}.yml'
+    if rhel.is_true_rhel():
+        return base_dir / f'rhel{rhel.major}-playbook-{profile}.yml'
+    elif rhel.is_centos():
+        if rhel <= 8:
+            return base_dir / f'centos{rhel.major}-playbook-{profile}.yml'
+        else:
+            return base_dir / f'cs{rhel.major}-playbook-{profile}.yml'
     else:
-        raise RuntimeError("cannot find playbook for non-RHEL")
+        raise RuntimeError("cannot find playbook for testing")
 
 
 def get_kickstart(profile):
     base_dir = Path('/usr/share/scap-security-guide/kickstart')
-    if versions.rhel:
-        return base_dir / f'ssg-rhel{versions.rhel.major}-{profile}-ks.cfg'
+    # RHEL and CentOS Stream both use 'ssg-rhel*' files
+    if rhel:
+        return base_dir / f'ssg-rhel{rhel.major}-{profile}-ks.cfg'
     else:
-        raise RuntimeError("cannot find kickstart for non-RHEL")
+        raise RuntimeError("cannot find kickstart for testing")
