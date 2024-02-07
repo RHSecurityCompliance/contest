ad04ccd4de04407eaf91dc2be9af8c99e432a232
Jiri Jaburek <comps@nomail.dom>
2024-02-07 16:11:38 +0100

    remove TODO excludes in conf.remediation
    These were added from a former Beakerlib test and have not proved
    to be relevant to Contest, and likely never will.
    
    Plus, this fixes a bug for ansible tests which had 'excludes'
    overriden with an empty list (a '=' was used, not '+='), though
    since no code was above 'Hardenings via Ansible', the bug never
    manifested.
    
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- conf/remediation.py
+++ conf/remediation.py
@@ -14,26 +14,6 @@ test_name = util.get_test_name()
 
 # Hardenings via Ansible
 if re.fullmatch('/hardening.*/ansible/.*', test_name):
-    excludes = [
-        # TODO:
-        #'ensure_gpgcheck_globally_activated',
-        #'ensure_gpgcheck_local_packages',
-        #'ensure_gpgcheck_never_disabled',
-        #'ensure_gpgcheck_repo_metadata',
-        #'ensure_redhat_gpgkey_installed',
-        #'no_direct_root_logins',
-        #'set_firewalld_default_zone',
-        #'firewalld_sshd_disabled',
-        #'service_sshd_disabled',
-        #'iptables_sshd_disabled',
-        #'sshd_disable_root_login',
-        #'sshd_disable_empty_passwords',
-        #'disable_host_auth',
-        #'harden_sshd_crypto_policy',
-        #'configure_etc_hosts_deny',
-        #'sudo_add_noexec',
-        #'accounts_password_set_max_life_existing',
-    ]
     if versions.rhel.is_centos():
         excludes += [
             # https://github.com/ComplianceAsCode/content/issues/8480
