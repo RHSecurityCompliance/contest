654c7635dda36f3b4a167e6256aeeeaabb35b77b
Jiri Jaburek <comps@nomail.dom>
2024-07-16 20:27:14 +0200

    explicitly generate RSA SSH keys
    RHEL-10 defaults to ed25519 which is incompatible with FIPS
    
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- lib/util/ssh.py
+++ lib/util/ssh.py
@@ -5,5 +5,5 @@ from lib import util
 
 def ssh_keygen(path):
     """Generate private/public keys prefixed by 'path'."""
-    cmd = ['ssh-keygen', '-N', '', '-f', path]
+    cmd = ['ssh-keygen', '-t', 'rsa', '-N', '', '-f', path]
     util.subprocess_run(cmd, stdout=subprocess.DEVNULL, check=True)
