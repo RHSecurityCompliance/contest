5d514757adde366abd58106771e9bc89cb33bec0
Jiri Jaburek <comps@nomail.dom>
2024-08-15 14:35:49 +0200

    move image-builder tailoring to the test
    This makes it more consistent with /hardening/kickstart
    and others.
    
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- hardening/image-builder/test.py
+++ hardening/image-builder/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from lib import results, oscap, osbuild, util
+from conf import remediation
 
 
 osbuild.Host.setup()
@@ -9,28 +10,30 @@ g = osbuild.Guest()
 
 profile = util.get_test_name().rpartition('/')[2]
 
-ds = util.get_datastream()
+oscap.unselect_rules(util.get_datastream(), 'remediation-ds.xml', remediation.excludes())
 
-# provide our modified DS via RpmPack to the VM as /root/contest-ds.xml,
+# provide our modified DS via RpmPack to the VM as /root/remediation-ds.xml,
 # tell the 'oscap xccdf eval --remediate' called by osbuild-composer to use it
 rpmpack = util.RpmPack()
-rpmpack.add_file(ds, '/root/contest-ds.xml')
+rpmpack.add_file('remediation-ds.xml', '/root/remediation-ds.xml')
 
 cmd = [
     'oscap', 'xccdf', 'generate', '--profile', profile,
     'fix', '--fix-type', 'blueprint',
-    ds,
+    'remediation-ds.xml',
 ]
 _, lines = util.subprocess_stream(cmd, check=True)
-blueprint = osbuild.translate_oscap_blueprint(lines, profile, '/root/contest-ds.xml')
+blueprint = osbuild.translate_oscap_blueprint(lines, '/root/remediation-ds.xml')
 
 g.create(blueprint=blueprint, rpmpack=rpmpack)
 
 with g.booted():
+    # copy the original DS to the guest
+    g.copy_to(util.get_datastream(), 'scan-ds.xml')
     # scan the remediated system
     proc, lines = g.ssh_stream(
         f'oscap xccdf eval --profile {profile} --progress --report report.html'
-        f' --results-arf results-arf.xml /root/contest-ds.xml'
+        f' --results-arf results-arf.xml scan-ds.xml'
     )
     oscap.report_from_verbose(lines)
     if proc.returncode not in [0,2]:
