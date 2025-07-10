97c3e25ed3cfc3a042cba4a234e98c7c1343b5d7
Jan Černý <jcerny@redhat.com>
2025-07-10 15:19:30 +0200

    Add a test for Ansible check mode
    This test will test whether our Ansible Playbooks can run
    in check mode (`ansible-playbook --check`).

---
+++ static-checks/ansible/check-mode/test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+
+from lib import util, results, ansible
+
+
+profile = util.get_test_name().rpartition('/')[2]
+
+# the VM guest ssh code doesn't use $HOME/.known_hosts, so Ansible blocks
+# on trying to accept its ssh key - tell it to ignore this
+os.environ['ANSIBLE_HOST_KEY_CHECKING'] = 'False'
+
+ansible.install_deps()
+
+pack = util.RpmPack()
+pack.add_sshd_late_start()
+pack.install()
+
+playbook = util.get_playbook(profile)
+ansible_cmd = [
+    'ansible-playbook', '-v', '-c', 'local', '-i', 'localhost,', '--check',
+    playbook,
+]
+_, lines = util.subprocess_stream(ansible_cmd, stderr=subprocess.STDOUT, check=True)
+ansible.report_from_output(lines, to_file='ansible-playbook.log')
+util.subprocess_run(['gzip', '-9', 'ansible-playbook.log'], check=True)
+results.report_and_exit(logs=['ansible-playbook.log.gz'])
