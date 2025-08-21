e3baaa084036006e3c6802e32f526b872d6e5d73
Jan Černý <jcerny@redhat.com>
2025-08-21 11:44:44 +0200

    Apply code review suggestions

--- hardening/container/day2-upgrade/test.py
+++ hardening/container/day2-upgrade/test.py
@@ -37,28 +37,29 @@ pack.add_sshd_late_start()
 with pack.build() as pack_binrpm:
     shutil.copy(pack_binrpm, 'contest-pack.rpm')
 
-with util.get_old_datastream() as old_xml:
-    # prepare a Container file for making a hardened image using the old data stream
-    oscap.unselect_rules(old_xml, 'remediation-old.xml', remediation.excludes())
-    cfile = podman.Containerfile()
-    cfile += util.dedent(fr'''
-        FROM {src_image}
-        # install testing-specific RpmPack
-        COPY contest-pack.rpm /root/.
-        RUN dnf -y install /root/contest-pack.rpm && rm -f /root/contest-pack.rpm
-        # copy over testing-specific datastream
-        COPY remediation-old.xml /root/
-        # install and run oscap-im to harden the image
-        RUN dnf -y install openscap-utils
-        RUN oscap-im --profile '{profile}' \
-            --results-arf /root/remediation-old-arf.xml /root/remediation-old.xml
-        RUN bootc container lint
-    ''')
-    cfile.add_ssh_pubkey(guest.ssh_pubkey)
-    cfile.write_to('Containerfile')
+# prepare a Container file for making a hardened image using the old data stream
+with util.get_old_datastream() as old_ds:
+    oscap.unselect_rules(
+        old_ds, 'remediation-old-ds.xml', remediation.excludes())
+cfile = podman.Containerfile()
+cfile += util.dedent(fr'''
+    FROM {src_image}
+    # install testing-specific RpmPack
+    COPY contest-pack.rpm /root/.
+    RUN dnf -y install /root/contest-pack.rpm && rm -f /root/contest-pack.rpm
+    # copy over testing-specific datastream
+    COPY remediation-old-ds.xml /root/
+    # install and run oscap-im to harden the image
+    RUN dnf -y install openscap-utils
+    RUN oscap-im --profile '{profile}' \
+        --results-arf /root/remediation-old-arf.xml /root/remediation-old-ds.xml
+    RUN bootc container lint
+''')
+cfile.add_ssh_pubkey(guest.ssh_pubkey)
+cfile.write_to('Containerfile')
 
-    podman.podman('pull', src_image)
-    podman.podman('image', 'build', '--tag', 'contest-hardened-old', '.')
+podman.podman('pull', src_image)
+podman.podman('image', 'build', '--tag', 'contest-hardened-old', '.')
 
 # prepare a Container file for making a hardened image using the new data stream
 oscap.unselect_rules(util.get_datastream(), 'remediation-new-ds.xml', remediation.excludes())
@@ -73,7 +74,7 @@ cfile2 += util.dedent(fr'''
     # install and run oscap-im to harden the image
     RUN dnf -y install openscap-utils
     RUN oscap-im --profile '{profile}' \
-        --results-arf /root/remediation-arf.xml /root/remediation-new-ds.xml
+        --results-arf /etc/remediation-new-arf.xml /root/remediation-new-ds.xml
     RUN bootc container lint
 ''')
 cfile2.add_ssh_pubkey(guest.ssh_pubkey)
@@ -114,16 +115,13 @@ with podman.Registry(host_addr=virt.NETWORK_HOST) as registry:
     raddr, rport = registry.get_listen_addr()
     # boot up and scan the VM
     with guest.booted():
-        guest.copy_to(util.get_datastream(), 'scan-ds.xml')
-
         guest.ssh(
             fr'''echo -e '[[registry]]\nlocation = "{raddr}:{rport}"\n'''
             r'''insecure = true\n' >> /etc/containers/registries.conf''',
         )
 
         # Run "bootc switch" to switch the image to the new one
-        proc, lines = guest.ssh_stream(f"bootc switch {raddr}:{rport}/contest-hardened-new")
-        print("\n".join(lines))
+        guest.ssh(f"bootc switch {raddr}:{rport}/contest-hardened-new", check=True)
 
         # reboot the VM to apply the new image
         # we can't use guest.soft_reboot() here, because we don't have a guest agent
@@ -133,6 +131,7 @@ with podman.Registry(host_addr=virt.NETWORK_HOST) as registry:
         virt.wait_for_ssh(guest.ipaddr)
 
         # copy the original DS to the guest
+        guest.copy_to(util.get_datastream(), 'scan-ds.xml')
         # scan the remediated system
         proc, lines = guest.ssh_stream(
             f'oscap xccdf eval --profile {profile} --progress --report report.html'
@@ -143,9 +142,12 @@ with podman.Registry(host_addr=virt.NETWORK_HOST) as registry:
             raise RuntimeError(f"post-reboot oscap failed unexpectedly with {proc.returncode}")
         guest.copy_from('report.html')
         guest.copy_from('scan-arf.xml')
+        guest.copy_from('remediation-old-arf.xml')
+        guest.copy_from('/etc/remediation-new-arf.xml')
 
 tar = [
     'tar', '-cvJf', 'results-arf.tar.xz', 'scan-arf.xml', 'report.html',
+    'remediation-old-arf.xml', 'remediation-new-arf.xml',
 ]
 util.subprocess_run(tar, check=True)
 
