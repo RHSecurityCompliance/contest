53a3229fc1b1d7299b49ae1a50da452189eabf89
macko1 <44851174+macko1@users.noreply.github.com>
2026-04-24 12:19:58 +0200

    fix ansible-galaxy locale error in per-rule %post

--- per-rule/test.py
+++ per-rule/test.py
@@ -142,9 +142,13 @@ with util.get_source_content() as content_dir:
             EOF
         '''))
     else:
+        # Set the locale to fix the error:
+        #   Ansible requires the locale encoding to be UTF-8; Detected None.
+        # The locale has to be set in the %post because it is running in a separate
+        # sibling process to the installer, not in its child process.
         ks.add_post(util.dedent('''
-            ansible-galaxy collection install community.general
-            ansible-galaxy collection install ansible.posix
+            export LC_ALL=en_US.UTF-8
+            ansible-galaxy collection install community.general ansible.posix
         '''))
 
     # install the VM
