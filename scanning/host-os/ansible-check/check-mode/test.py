3554a5ff17369805604f07d529dc9c02aa8b204d
Matus Marhefka <mmarhefk@redhat.com>
2026-04-27 10:18:53 +0200

    Report issues in tested functionality as fail instead of error
    Resolves #518

--- scanning/host-os/ansible-check/check-mode/test.py
+++ scanning/host-os/ansible-check/check-mode/test.py
@@ -22,8 +22,8 @@ ansible_cmd = [
     *skip_tags_arg, playbook,
 ]
 proc, lines = util.subprocess_stream(ansible_cmd, stderr=subprocess.STDOUT)
-ansible.report_from_output(lines, to_file='ansible-playbook.log', failure='fail')
-results.report_and_exit(
-    'pass' if proc.returncode == 0 else 'fail',
-    logs=['ansible-playbook.log'],
-)
+ansible.report_from_output(lines, to_file='ansible-playbook.log')
+results.add_log('ansible-playbook.log')
+if proc.returncode != 0:
+    results.report_and_exit('fail', note=f"ansible-playbook failed with {proc.returncode}")
+results.report_and_exit()
