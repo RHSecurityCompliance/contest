7dc0dc98e7ed8f9b2637297e23574ee2dfeee3f9
Jan Černý <jcerny@redhat.com>
2026-04-30 13:37:35 +0200

    Extend Hummingbird scanning test
    This commit changes the `/scanning/hummingbird` tests with
    these 2 major improvements:
    
    1. The test has been split into 2 tests:
       1. scanning using oscap-podman
       2. scanning using the openscap hummingbird container image
    2. The test now tests more profile and image variants:
       1. normal images are scanned with CIS profiles
       2. FIPS images are scanned with both CIS and STIG profiles
    
    The reason for the improvement 1 is that while users can use
    oscap-podman for scanning, the hummingbird users will be guided
    by documentation to use the scanner and content shipped in
    the `quay.io/hummingbird/openscap` container image.
    
    The reason for the improvement 2 CIS profile should be applicable
    also on FIPS variants of images. Remember that hummingbird provides
    images in 2 basic variants:
    1. normal (eg. `:latest`) - compliant only with CIS
    2. FIPS (eg. `:latest-fips`) - compliant with CIS and STIG
    
    Related to: https://github.com/ComplianceAsCode/content/pull/14683

---
+++ scanning/hummingbird/image/test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import shutil
+import subprocess
+import tempfile
+
+from lib import results, metadata, oscap, podman, util
+from pathlib import Path
+
+IMAGE = 'openjdk'
+profile = util.get_test_name().rpartition('/')[2]
+if 'fips' in metadata.tags():
+    image_variant = 'latest-fips'
+else:
+    image_variant = 'latest'
+image_id = f'quay.io/hummingbird/{IMAGE}:{image_variant}'
+podman.podman('pull', image_id)
+
+with util.get_source_content() as content_dir:
+    # Hummingbird is a separate product in ComplianceAsCode/content
+    util.build_content(
+        content_dir,
+        {
+            'SSG_PRODUCT_HUMMINGBIRD:BOOL': 'ON',
+        },
+    )
+    ds_path = content_dir / util.CONTENT_BUILD_DIR / 'ssg-hummingbird-ds.xml'
+    if not ds_path.exists():
+        raise RuntimeError(f"Datastream not found: {ds_path}")
+    with tempfile.TemporaryDirectory() as tempdir:
+        shutil.copy(ds_path, tempdir)
+        proc, lines = util.subprocess_stream(
+            [
+                'podman', 'run', '--rm', '--cap-add', 'SYS_CHROOT', '--mount',
+                f'type=image,source={image_id},destination=/target', '-e',
+                'OSCAP_PROBE_ROOT=/target', '-v', f'{tempdir}:/ssg:z,U',
+                'quay.io/hummingbird/openscap:latest', 'xccdf', 'eval',
+                '--progress', '--profile', profile, '--results-arf',
+                '/ssg/scan-arf.xml', '--report', '/ssg/report.html',
+                '/ssg/ssg-hummingbird-ds.xml',
+            ],
+            stderr=subprocess.STDOUT,
+        )
+        oscap.report_from_verbose(lines)
+        if proc.returncode not in [0, 2]:
+            raise RuntimeError("oscap failed unexpectedly")
+        shutil.copy(tempdir + '/scan-arf.xml', Path.cwd())
+        shutil.copy(tempdir + '/report.html', Path.cwd())
+
+results.report_and_exit(logs=['report.html', 'scan-arf.xml'])
