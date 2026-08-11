0b6273698dc91830288ff1972abd673b0e90b22b
Jiri Jaburek <comps@nomail.dom>
2026-08-11 10:06:32 +0200

    always test per-rule playbooks in user content
    This impacts
    
      /static-checks/ansible/lint
      /static-checks/ansible/syntax-check
    
    by making them always test per-rule playbooks:
    
    - if CONTEST_CONTENT is given, the playbooks are explicitly built
      (or reused if built already)
    
    - else we're testing RPM content and rely on per-rule-playbook RPM
      to be installed via 'recommend'
    
      - we could enforce its presence, but we probably want the tests
        usable even when it's not available, so RPM testing is still
        opportunistic
    
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- static-checks/ansible/syntax-check/test.py
+++ static-checks/ansible/syntax-check/test.py
@@ -5,32 +5,35 @@ import subprocess
 from lib import ansible, util, results
 
 
+def check_playbook(playbook, name):
+    cmd = ['ansible-playbook', '--syntax-check', playbook]
+    ret = util.subprocess_run(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+    )
+    if ret.returncode == 0:
+        results.report('pass', name)
+    else:
+        results.report('fail', name, ret.stdout)
+
+
 ansible.install_deps()
 
 ds = util.get_datastream()
 
-# Generate playbook from datastream and virtual (all) profile
+# Check playbook from datastream and virtual (all) profile
 cmd = [
     'oscap', 'xccdf', 'generate', 'fix', '--profile', '(all)',
     '--fix-type', 'ansible', '--output', 'playbook.yml', ds,
 ]
 ret = util.subprocess_run(cmd, check=True, stderr=subprocess.PIPE)
+check_playbook('playbook.yml', '(all) profile generated')
 
-# Check syntax of generated playbook
-cmd = ['ansible-playbook', '--syntax-check', 'playbook.yml']
-ret = util.subprocess_run(cmd, stderr=subprocess.PIPE)
-if ret.returncode == 0:
-    results.report('pass', '(all) profile generated')
-else:
-    results.report('fail', '(all) profile generated', ret.stderr)
-
-# Check syntax of shipped playbooks
+# Check shipped playbooks
 for playbook in util.iter_playbooks():
-    cmd = ['ansible-playbook', '--syntax-check', playbook]
-    ret = util.subprocess_run(cmd, stderr=subprocess.PIPE)
-    if ret.returncode == 0:
-        results.report('pass', playbook.name)
-    else:
-        results.report('fail', playbook.name, ret.stderr)
+    check_playbook(playbook, playbook.name)
+
+# Check per-rule playbooks
+for playbook in util.iter_per_rule_playbooks():
+    check_playbook(playbook, playbook.name)
 
 results.report_and_exit()
